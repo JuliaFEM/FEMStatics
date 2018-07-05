7328d4ef2bf713312c05317d95ad8fda338badfc
autodocs <autodocs>
2018-07-05 11:59:36 +0000

    build based on 99643b9

---
+++ latest/search_index.js
@@ -0,0 +1,19 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Documentation of FEMStatics.jl",
+    "title": "Documentation of FEMStatics.jl",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Documentation-of-FEMStatics.jl-1",
+    "page": "Documentation of FEMStatics.jl",
+    "title": "Documentation of FEMStatics.jl",
+    "category": "section",
+    "text": "Nonlinear quasistatic analysis for JuliaFEM.DocTestSetup = quote\n    using FEMBase\n    using FEMStatics\nend"
+},
+
+]}
